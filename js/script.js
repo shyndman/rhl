f3f2e258dcaa49b654929476ef309e4313c94ee0
shyndman <scotty.hyndman@gmail.com>
2011-03-23 21:37:08 -0400

    * Wrote some basic js to handle nav changes (needs to include animation support, and history mgmt) * Added a spacer gif to portfolio

--- js/script.js
+++ js/script.js
@@ -2,84 +2,41 @@
 $(function() {
 	// For testing without js:
 	// return;
-		
-	// CONTENT SCROLLING
 	
-	var scroller = $("#slide-scroller").flow();
+	var activeLink = $('#content nav a.active');
+	$('#content nav a').each(function() {
+	  if ($(this).hasClass('active'))
+	    return;
+	    
+	  $($(this).attr('href')).hide();
+	}).click(function() {
+	  // Div hiding (to be replaced with animation)
+	  $(activeLink.attr('href')).hide();
+	  $($(this).attr('href')).show();
+	  
+	  // Style links
+	  styleInactiveLink(activeLink);
+	  styleActiveLink(activeLink = $(this));
+	  
+	  return false;
+	});
 	
-	// NAVIGATION
-	
-	// Page title setup
-	var pageTitleBase = document.title;
-	pageTitleBase += " - ";
-	
-	var navLinks = $("header nav a");
-	navLinks.click(function(eventObj) {
-		var self = $(this);
-				
-		// selected nav link
-		navLinks.removeClass("selected");
-		self.addClass("selected");
-		
-		// page title
-		document.title = pageTitleBase + self.data("page-title");
-		
-		// grab the id to scroll to, which is in the href after the first character (#)
-		scroller.flow("slideToId", $(this).attr("href").substring(1));
-		
-		return false;
-	}).first().click();
-	
-	// PORTFOLIO
-	
-	var setSelectedPhoto = function(photo) {
-		console.log(photo.image);
-		
-		var fullImg = $("#portfolio-selected-img > img");
-		fullImg.attr("src", photo.project.baseDir + "/" + photo.image + ".jpg");
-	};
-	
-	var setSelectedProject = function(project) {
-		var thumbHolder = $("#portfolio-thumbnails > ul").empty();
+	/** Styles a page navigation link as active */
+	var styleActiveLink = function(link) {
+	  if (link.hasClass('active'))
+	    return;
+	    
+	  link.addClass('active');
+	  link.text('[' + link.text() + ']');
+	}
 
-		var thumbMap = _.reduce(project.photos, function(memo, photo) {
-			photo.project = project;
-			memo[photo.image] = photo;
-			return memo;
-		}, {});
-
-		// Add the thumbnails
-		$("#portfolio-thumb-tmpl").tmpl(
-			project.photos, {baseDir: project.baseDir}).appendTo(thumbHolder);
-			
-		// Add click handlers to the thumbs
-		thumbHolder.find("a").click(function() {
-			$(this).parent().siblings().removeClass("selected");
-			$(this).parent().addClass("selected");
-			
-			setSelectedPhoto(thumbMap[$(this).data("key")]);
-			return false;
-		}).first().click();
+	/** Styles a page navigation link as inactive */	
+	var styleInactiveLink = function(link) {
+	  if (!link.hasClass('active'))
+	    return;
+	    
+    link.removeClass('active');
+    var curText = link.text();
+    link.text(curText.substr(1, curText.length - 2))
 	};
-	
-	var setPortfolio = function(portfolio) {
-		// Get a project map, keyed on name, for project selection
-		var projectMap = _.reduce(portfolio, function(memo, project) {
-			memo[project.name] = project;
-			return memo;
-		}, {});
-		
-		$("#portfolio-project-link-tmpl").tmpl(portfolio).appendTo("#portfolio-projects");
-		
-		$("#portfolio-projects a").click(function() {
-			setSelectedProject(projectMap[$(this).text()]);
-			return false;
-		});
-		
-		setSelectedProject(portfolio[0]);
-	};
-	
-	// Load portfolio JSON
-		
-	$.getJSON("portfolio.json", setPortfolio);
 });
