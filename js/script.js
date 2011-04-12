a4b530860ba49b56136e36374183c3b29be2e804
shyndman <scotty.hyndman@gmail.com>
2011-04-12 08:45:28 -0400

    * Styling tweaks * Removal of unused images * Addition of design portfolio * Script.js now includes GA insertion

--- js/script.js
+++ js/script.js
@@ -67,14 +67,13 @@ $(function() {
   $(".gallery-link").click(function() {
     var galKey = $(this).data("gal-key");
     var gallery = window.galleryInfo[galKey];
-    console.log(galKey + ", " + gallery);
     
     $.fancybox(gallery.slice(), {
       'padding'           : 0,
       'transitionIn'      : 'fade',
       'transitionOut'     : 'none',
       'type'              : 'image',
-      'changeFade'        : 100
+      'changeFade'        : 0
     });
   });
 	
@@ -86,3 +85,17 @@ $(function() {
     selectPage(hash == "" ? "home" : hash);
   }, { unescape: ",/" });
 });
+
+//
+// Analytics (run outside site ready)
+//
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-22674522-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
